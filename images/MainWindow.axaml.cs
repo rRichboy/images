f8f34837f98b05bfb7e4fdbee38897d4c6cfa45c
rRichboy <tvinktvink893@gmail.com>
2023-11-14 13:57:48 +0300

    update + database

--- images/MainWindow.axaml.cs
+++ images/MainWindow.axaml.cs
@@ -1,47 +1,68 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using SkiaSharp;
-using System.Diagnostics.Metrics;
-using Tmds.DBus.Protocol;
-using Avalonia.Media.Imaging;
 using Avalonia.Media;
-using Avalonia;
+using Avalonia.Media.Imaging;
 using System;
 
 namespace images
 {
     public partial class MainWindow : Window
     {
-        private string[] my_pathes = new string[] { "C:/cat1.jpg", "C:/cat2.jpg", "C:/cat3.jpg" };
+        //массивы картинки + описания
+        private string[] my_pathes = new string[] { "avares://images/Assets/cat1.jpg", "avares://images/Assets/cat2.jpg", "avares://images/Assets/cat3.jpg" };
         private string[] imageDescriptions = new string[] { "Этот мявчик нашкодил", "Этот мявчик удивленный", "Этот мявчик только проснулся" };
         private int now_pic = 0;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        //изначально скрыты далее по нажатию кнопки крутит по одной
         public void ShowPic(object sender, RoutedEventArgs args)
         {
-            myPic.IsVisible = true;
+            myPic1.IsVisible = false;
+            myPic2.IsVisible = false;
+            myPic3.IsVisible = false;
+
             now_pic = now_pic + 1;
             if (now_pic == 3) now_pic = 0;
-            myPic.Source = new Bitmap (my_pathes[now_pic]);
+
+            switch (now_pic)
+            {
+                case 0:
+                    myPic1.IsVisible = true;
+                    break;
+                case 1:
+                    myPic2.IsVisible = true;
+                    break;
+                case 2:
+                    myPic3.IsVisible = true;
+                    break;
+            }
 
             imageText.IsVisible = true;
-
             imageText.Text = imageDescriptions[now_pic];
         }
 
+        //скрыть блохастых по нажатию кнопки
         public void HidePic(object sender, RoutedEventArgs args)
         {
-            myPic.IsVisible = false;
+            myPic1.IsVisible = false;
+            myPic2.IsVisible = false;
+            myPic3.IsVisible = false;
+
             imageText.IsVisible = false;
         }
 
+        //выход
         public void Exit(object sender, RoutedEventArgs args)
         {
             Environment.Exit(0);
         }
 
+        //переход на новое окно
         public void Regs(object sender, RoutedEventArgs args)
         {
             new Window1().Show();
--- images/Window1.axaml.cs
+++ images/Window1.axaml.cs
@@ -2,54 +2,170 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using Avalonia.Skia.Helpers;
 using System;
+using System.Drawing;
+using Npgsql;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Avalonia.Controls.Shapes;
 
-namespace images;
-
-public partial class Window1 : Window
+namespace images
 {
-    private TextBox passwordTextBox;
-    private Button showPasswordButton;
-
-    public Window1()
+    public partial class Window1 : Window
     {
-        InitializeComponent();
-    }
+        private readonly NpgsqlConnection conn;
 
-    private void Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {
-        RegistrationTabControl.IsSelected = true;
-    }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
-        passwordTextBox = this.Find<TextBox>("Pass");
-        showPasswordButton = this.Find<Button>("ShowPasswordButton");
-
-        if (showPasswordButton != null)
+        public Window1()
         {
-            showPasswordButton.Click += ShowPassword_Click;
-        }
-    }
+            //получение пути к файлу settings.json
+            string path = Directory.GetCurrentDirectory();
+            string projectPath = Directory.GetParent(path).Parent.Parent.FullName;
+            string settingsFile = projectPath + @"\settings.json";
+            string? connectionString = "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=postgres";
 
-    private void ShowPassword_Click(object sender, RoutedEventArgs e)
-    {
-        if (passwordTextBox != null)
+            if (File.Exists(settingsFile))
+            {
+                //чтение файла и его разбор 
+                JObject? jobj = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(settingsFile));
+
+                //получение строки подключения
+                connectionString = jobj["ConnectionString"]?["DefaultConnection"]?.ToString();
+
+            } 
+            
+            conn = new NpgsqlConnection(connectionString);
+            
+            InitializeComponent();
+        }
+
+        //на вкладке "Войти" нажимаем регистриция и перекидывает на "Регистриция"
+        private void Button_Click3(object sender, RoutedEventArgs e)
         {
-            if (passwordTextBox.PasswordChar == '\0')
+            RegistrationTabControl.IsSelected = true;
+        }
+
+        //показ и скрытие пароля при регистрации по нажатию "*"
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Pass != null)
             {
-                passwordTextBox.PasswordChar = '*';
-            }
-            else
-            {
-                passwordTextBox.PasswordChar = '\0';
+                if (Pass.PasswordChar == '\0')
+                {
+                    Pass.PasswordChar = '*';
+                }
+                else
+                {
+                    Pass.PasswordChar = '\0';
+                }
             }
         }
-    }
 
-    public void Exit1(object sender, RoutedEventArgs args)
-    {
-        Environment.Exit(0);
+        //кнопка "выйти" на вкладке "Выйти"
+        public void Exit1(object sender, RoutedEventArgs args)
+        {
+            Environment.Exit(0);
+        }
+
+        //вкладка "Регистрация" кнопка зарегистрироваться
+        private async void Button_Click_100(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            try
+            {
+                await conn.OpenAsync();
+
+                //инсертим данные при регистрации
+                await using (var cmd = new NpgsqlCommand("INSERT INTO users (login, pwd, date) VALUES (@login, @pwd, @date)", conn))
+                {
+                    var loginTextBox = this.FindControl<TextBox>("Login");
+                    var passwordTextBox = this.FindControl<TextBox>("Pass");
+
+                    cmd.Parameters.AddWithValue("login", loginTextBox.Text);
+                    cmd.Parameters.AddWithValue("pwd", passwordTextBox.Text);
+                    cmd.Parameters.AddWithValue("date", datePick.SelectedDate.Value.DateTime);
+
+                    await cmd.ExecuteNonQueryAsync();
+
+                    //если все красиво, то сообщение:
+                    ErEx.Text = "Регистрация успешна!";
+                }
+            }
+            catch (Exception ex)
+            {
+                //если все не очень красиво, то сообщение:
+                ErEx.Text = $"Ошибка регистрации: {ex.Message}";
+
+            }
+            finally
+            {
+                //закрываем соединение с бд-шкой
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        //вкладка "Войти" кнопка войти
+        private async void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            var usernameTextBox = this.FindControl<TextBox>("UsernameTextBox");
+            var passwordTextBox = this.FindControl<TextBox>("PasswordTextBox");
+
+            string username = usernameTextBox.Text;
+            string password = passwordTextBox.Text;
+
+            try
+            {
+                await conn.OpenAsync();
+
+                //чекаем валидность данных в бд
+                await using (var cmd = new NpgsqlCommand("SELECT COUNT(*) FROM users WHERE login = @login AND pwd = @pwd", conn))
+                {
+                    cmd.Parameters.AddWithValue("login", username);
+                    cmd.Parameters.AddWithValue("pwd", password);
+
+                    int count = Convert.ToInt32(await cmd.ExecuteScalarAsync());
+
+                    if (count > 0)
+                    {
+                        //если все красиво, то сообщение + ивенты:
+                        LoginTabControl.IsVisible = false;
+                        RegistrationTabControl.IsVisible = false;
+                        MeowForAuthBlock.IsVisible = false;
+                        Private.IsSelected = true;
+                        myPic4.IsVisible = true;
+
+                        ErEx0.Text = "Вход выполнен успешно!";
+                    }
+                    else
+                    {
+                        //мискликнул
+                        ErEx0.Text = "Неверный логин или пароль.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //если все не очень красиво, то сообщение:
+                ErEx0.Text = $"Ошибка входа: {ex.Message}";
+            }
+            finally
+            {
+                //закрываем соединение с бд-шкой
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        //на вкладке "Войти" нажимаем регистриция и перекидывает на "Регистриция"
+        private void Button_Click_200(object sender, RoutedEventArgs e)
+        {
+            ErEx2.Text = "В разработке.";
+        }
     }
 }
